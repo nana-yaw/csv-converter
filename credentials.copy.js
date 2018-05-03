89cfe2fbd9c01b12ceb3c9c78e2b8a2d0462eb8a
Edward Osei-Nyarko <edward.osei-nyarko@persol.net>
2018-05-03 12:48:39 +0000

    Making a dummy DB Connection Credential File

---
+++ credentials.copy.js
@@ -0,0 +1,8 @@
+var credentials = {
+    user: "your user name",
+    password: "your password",
+    host: "your host IP, e.g. localhost or 127.0.0.1",
+    database: "your database name"
+}
+
+module.exports = credentials;
