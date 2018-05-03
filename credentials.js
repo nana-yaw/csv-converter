414f69a565236d6e1d0039142c5babe95e68faac
Edward Osei-Nyarko <edward.osei-nyarko@persol.net>
2018-05-03 12:52:00 +0000

    Security reasons

--- credentials.js
+++
@@ -1,8 +0,0 @@
-var credentials = {
-    user: "your user name",
-    password: "your password",
-    host: "your host IP, e.g. localhost or 127.0.0.1",
-    database: "your database name"
-}
-
-module.exports = credentials;
